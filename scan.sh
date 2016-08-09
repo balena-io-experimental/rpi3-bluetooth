83ff8f037ef9f65b04679fbe7e11e3b1fd8c97b1
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-08-09 09:27:38 +0100

    Initial scan testing

---
+++ scan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FAILED=0
+
+echo "Testing bluetooth on RPI3. Make sure you have a bluetooth device enabled and visible."
+
+echo "Attaching hci0..."
+if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+    echo "First try failed. Let's try another time."
+    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+fi
+
+echo "Bring hci0 up..."
+hciconfig hci0 up
+
+echo "Scan for devices..."
+if [ `hcitool scan | wc -l` -le 1 ]; then
+    FAILED=1
+else
+    FAILED=0
+fi
+
+echo "Test finished. App configured to not exit. Restart the app if you want to retest."
+
+# Test result
+if [ $FAILED -eq 1 ]; then
+    echo "TEST FAILED"
+else
+    echo "TEST PASSED"
+fi
+
+# Don't exit the process
+while true; do
+    sleep 1
+done
